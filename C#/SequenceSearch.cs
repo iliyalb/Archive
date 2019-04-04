ee58161ffd559b7e37ab85de4dc1604f4a89e4be
ILIYA LESANI <iliyalesani@gmail.com>
2019-04-04 18:06:46 +0430

    Add C#

---
+++ C#/SequenceSearch.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Program
+{
+    class Sequence
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 1, 2, 3, 4, 5 };
+            SequenceSearch(a, 3);
+        }
+
+        public static void SequenceSearch(int[] a, int x)
+        {
+            int n = a.Length;
+
+            for (int i = 0; i < n; i++)
+                if (x == a[i])
+                {
+                    Console.WriteLine("{0} ---> A[{1}]", x, i);
+                    return;
+                }
+
+            Console.WriteLine("Not found");
+        }
+    }
+}
