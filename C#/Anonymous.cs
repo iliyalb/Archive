e33d058dfdaaa02a865b15edfb663ff9c4dda28e
ILIYA LESANI <iliyalesani@gmail.com>
2019-04-04 18:51:24 +0430

    Add C#

---
+++ C#/Anonymous.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Program
+{
+    internal class Anonymous
+    {
+        private delegate void MyDelegate(int end);
+
+        static void Main(string[] args)
+        {
+            MyDelegate count = delegate (int end)
+            {
+                for (int i = 0; i <= end; i++)
+                    Console.WriteLine(i);
+            };
+
+            count(10);
+        }
+    }
+}
