cfd14a66560900905b8e953d64eb49a34cbc1c9d
ILIYA LESANI <iliyalesani@gmail.com>
2019-09-26 20:29:52 +0330

    Add Company

---
+++ ASP/Model.Company.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Learning_EFCF.Models
+{
+    public class Company
+    {
+        public Company()
+        {
+
+        }
+        public int CompanyID { get; set; }
+        public string CompanyName { get; set; }
+    }
+}
---
+++ ASP/Model.CompanyContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Learning_EFCF.Models
+{
+    public class CompanyContext : System.Data.Entity.DbContext
+    {
+        public CompanyContext()
+        {
+
+        }
+        public System.Data.Entity.DbSet<Company> Companies { get; set; }
+    }
+}
