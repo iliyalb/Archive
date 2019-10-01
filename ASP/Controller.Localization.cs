204efeae59e5b05bb752feb197c3ff91b7b0fba9
ILIYA LESANI <iliyalesani@gmail.com>
2019-10-02 03:01:03 +0330

    Create Controller.Localization.cs

---
+++ ASP/Controller.Localization.cs
@@ -0,0 +1,22 @@
+﻿// ~/Infrastructures/BaseController.cs
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Infrastructures
+{
+    public class BaseController : Controller
+    {
+        public BaseController()
+        {
+            // Localization to show from ~/App_GlobalResources/ErrorMessages.fa-IR.resx
+            CultureInfo oci = new CultureInfo("fa-IR");
+            System.Threading.Thread.CurrentThread.CurrentCulture = oci;
+            System.Threading.Thread.CurrentThread.CurrentUICulture = oci;
+        }
+    }
+}
