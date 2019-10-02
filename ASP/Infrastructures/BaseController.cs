bcadcb6978b57a209e01d406a09a7433032425f6
ILIYA LESANI <iliyalesani@gmail.com>
2019-10-02 03:31:57 +0330

    Create BaseController.cs

---
+++ ASP/Infrastructures/BaseController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Infrastructures
+{
+    public class BaseController : Controller
+    {
+        public BaseController()
+        {
+            CultureInfo oci = new CultureInfo("en-US");
+            System.Threading.Thread.CurrentThread.CurrentCulture = oci;
+            System.Threading.Thread.CurrentThread.CurrentUICulture = oci;
+        }
+    }
+}
