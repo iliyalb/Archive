2ba36a90081a04696e197df7e9386aa3845a341b
ILIYA LESANI <iliyalesani@gmail.com>
2019-10-29 13:08:14 +0330

    Add error handling

---
+++ ASP/App_Code/Infrastructures/CustomHandleErrorAttribute.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+// ~/App_Code/Infrastructures/CustomHandleErrorAttribute.cs
+
+// Define filters.Add(new Infrastructures.CustomHandleErrorAttribute()); in ~/App_Start/FilterConfig.cs
+
+// Usage in controllers: [Infrastructures.CustomHandleError]
+namespace Infrastructures
+{
+    class CustomHandleErrorAttribute : System.Web.Mvc.HandleErrorAttribute
+    {
+        public CustomHandleErrorAttribute()
+        {
+            // ctor
+        }
+
+        public override void OnException(System.Web.Mvc.ExceptionContext filterContext)
+        {
+            base.OnException(filterContext);
+
+            System.IO.StreamWriter writer = new System.IO.StreamWriter(HttpContext.Current.Server.MapPath("~/App_Data/Log.txt"), true);
+
+            writer.WriteLine("Time: " + System.DateTime.Now + " IP Address: " + HttpContext.Current.Request.UserHostAddress + " Message: " +
+                filterContext.Exception.Message +  " Path: " + HttpContext.Current.Request.PhysicalPath);                
+            
+            writer.Dispose();
+        }
+    }
+}
---
+++ ASP/App_Start/FilterConfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// ~/App_Start/FilterConfig.cs
+
+namespace SOLUTION_NAME
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(System.Web.Mvc.GlobalFilterCollection filters)
+        {
+            // Default error handler
+            //filters.Add(new System.Web.Mvc.HandleErrorAttribute());
+            
+            // Custom error handler (higher priority)
+            filters.Add(new Infrastructures.CustomHandleErrorAttribute());
+        }
+    }
+}
