62c41c5d3f417e497565f15f03a687f57790510a
ILIYA LESANI <iliyalesani@gmail.com>
2020-01-01 18:05:56 +0330

    Create Method.java

---
+++ Java/Method.java
@@ -0,0 +1,9 @@
+public class Method{
+    public static void main(String[] args){
+        addExclamation("Hello, World");
+    }
+
+    public static void addExclamation(String s){
+        System.out.println(s + "!");
+    }
+}
