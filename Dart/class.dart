f7a8323cde922c685748a7393f7bdd82f9f8fc4f
iliya lesani <iliyalesani@gmail.com>
2020-07-01 13:50:41 +0430

    Update class.dart

--- Dart/class.dart
+++ Dart/class.dart
@@ -1,12 +1,14 @@
 void main() {
   User first = User('first', 'ui421g427');
-  User second = User('second', 'v09wgwe9');
-
   first.login();
+
+  User second = User('second', 'v09wgwe9');
   second.login();
 
-  Admin mod = Admin('mod', '0gw89f131!');
+  User guest = User.guest();
+  guest.login();
 
+  Admin mod = Admin('mod', '0gw89f131!');
   mod.announce();
 }
 
@@ -14,9 +16,19 @@ class User {
   String username;
   String password;
 
-  User(String username, String password) {
+  // Default constructor
+  /*User(String username, String password) {
     this.username = username;
     this.password = password;
+  }*/
+
+  // Shortcut
+  User(this.username, this.password);
+
+  // Named constructor
+  User.guest() {
+    username = 'guest';
+    password = ' ';
   }
 
   void login() {
