c81393210cebd0fab79cf16e96aca91cdf516705
iliya <iliyalesani@gmail.com>
2020-09-10 17:43:32 +0430

    Create LocalDateNow.java

---
+++ Java/LocalDateNow.java
@@ -0,0 +1,11 @@
+import java.time.LocalDate;
+
+import jdk.javadoc.internal.doclets.formats.html.SourceToHTMLConverter;
+
+public class LocalDateNow {
+    public static void main(String[] args)
+    {
+        LocalDate currentDate = LocalDate.now();
+        System.out.println(currentDate.getMonth());
+    }
+}
