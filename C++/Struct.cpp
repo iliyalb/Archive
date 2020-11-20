29ba1d114d6fd9a629cf78dc3c012af3299045cf
iliya <iliyalesani@gmail.com>
2020-11-20 19:41:31 +0330

    Update Struct.cpp

--- C++/Struct.cpp
+++ C++/Struct.cpp
@@ -1,28 +1,33 @@
 #include <iostream>
+#include <cstring>
 
 struct character
 {
 	int age;
 	char gender;
-	char* name;
+	char desc[15];
+	char *name;
+	std::string email = "example@mail.net";
 };
 
 int main()
 {
+	auto desc = "something";
 	character foo;
 
 	foo.age = 20;
 	foo.gender = 'm';
+	std::strcpy(foo.desc, desc);
 	foo.name = new char[80];
 
 	std::cout << "Enter a new name: ";
 	std::cin >> foo.name;
-	std::cout << "Name: " << foo.name << " , Age: " << foo.age << std::endl;
+	std::cout << "Name: " << foo.name << " , Age: " << foo.age << " , Description: " << foo.desc << ", Email: " << foo.email << std::endl;
 
 	delete foo.name;
 
 	// Another example
-	character* player;
+	character *player;
 	player = new character;
 
 	player->age = 18;
