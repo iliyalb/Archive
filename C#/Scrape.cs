cb43c2c052587b253b5e7c02637bcb62edbae17c
iliyalesani <iliyalesani@gmail.com>
2021-03-21 03:10:32 +0330

    Create Scrape.cs

---
+++ C#/Scrape.cs
@@ -0,0 +1,25 @@
+using System;
+using System.IO;
+using System.Net;
+
+public class Scrape
+{
+    public string ScrapeWebpage(string url)
+    {
+        return GetWebpage(url);
+    }
+
+    public string ScrapeWebpage(string url, string filepath)
+    {
+        string reply = GetWebpage(url);
+
+        File.WriteAllText(filepath, reply);
+        return reply;
+    }
+
+    private string GetWebpage(string url)
+    {
+        WebClient client = new WebClient();
+        return client.DownloadString(url);
+    }
+}
