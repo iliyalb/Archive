289c8b43a245fbc541e2b9df2392bf8fe4c66717
iliyalesani <iliyalesani@gmail.com>
2021-03-21 03:18:02 +0330

    Create example.sol

---
+++ Solidity/example.sol
@@ -0,0 +1,48 @@
+pragma solidity ^0.4.20;
+
+// contract is similar to a class in blockchain
+contract SpaceDoggos {
+
+    uint maxPlanetsPerSystem = 10;
+    uint minPlanetsPerSystem = 3;
+
+    uint planetCodeDigits = 7;
+    uint systemCodeDigits = 7;
+
+    uint planetCodeModulus = 10 ** planetCodeDigits;
+    uint systemCodeModulus = 10 ** systemCodeDigits;
+
+    struct Doggo {
+        string name;
+        uint8 breed;
+        uint8 color;
+        uint8 face;
+        uint8 costume;
+        uint coordX;
+        uint coordY;
+    }
+
+    // mapping is same as hashing a key value pair
+    mapping(address => Doggo) doggos;
+
+    function createDoggo(string _name, uint8 _breed, uint8 _color, uint8 _face, uint8 _costume) {
+        Doggo memory myDoggo = Doggo({
+            name: _name,
+            breed: _breed,
+            color: _color,
+            face: _face,
+            costume: _costume,
+            coordX: 0,
+            coordY: 0
+        });
+
+        doggos[msg.sender] = myDoggo;
+    }
+
+    // a function can return multiple values
+    function getSystemMap(uint _coordX, uint _coordY) returns(uint)
+    {
+      // Ethereum’s KECCAK-256 algorithm that produces a 256-bit hexadecimal number
+      return uint(keccak256(_coordX,_coordY));
+    }
+}
